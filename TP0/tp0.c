d2ae3a43d07d94c7fb1c7b726510568cdb36cbb0
Ezequiel Acosta <82551908+ezequiel-acosta@users.noreply.github.com>
2021-04-22 19:27:47 -0300

    Update tp0.c

--- TP0/tp0.c
+++ TP0/tp0.c
@@ -6,18 +6,52 @@
  * *****************************************************************/
 
 void swap(int *x, int *y) {
+    int valor_x = *x;	
+	*x = *y;
+	*y = valor_x;
 }
 
 
 int maximo(int vector[], int n) {
-    return -1;
+    if (n == 0) {
+		return -1;
+	}
+	int pos_max = 0;
+	for (int i = 0; i < n; i++) {
+		if (vector[i] > vector[pos_max]) {
+			pos_max = i;
+		}
+	}
+    return pos_max;
 }
 
 
 int comparar(int vector1[], int n1, int vector2[], int n2) {
-    return 0;
+    int menor_largo = n1;
+	if (n1 > n2) {
+		menor_largo = n2;
+	}
+	for (int i = 0; i < menor_largo; i++) {
+		if (vector1[i] < vector2[i]) {
+			return -1;
+		}
+		else if (vector1[i] > vector2[i]) {
+			return 1;
+		}
+	}
+	if (n1 < n2 || (n1 == 0 && n1 != n2)) {
+		return -1;
+	}
+	else if (n1 > n2 || (n2 == 0 && n1 != n2)) {
+		return 1;
+	}
+	return 0;
 }
 
 
 void seleccion(int vector[], int n) {
+    int pos_max;
+	for (int i = n; i > 0; i--) {
+		pos_max = maximo(vector, i);
+		swap(&vector[pos_max], &vector[i-1]);
 }
